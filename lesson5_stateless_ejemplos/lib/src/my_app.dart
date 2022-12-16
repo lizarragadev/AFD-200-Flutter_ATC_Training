4393665b89dd19c9448e49bcff4bac9cce9779da
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-12-16 18:49:05 -0400

    Lesson stateless

---
+++ lesson5_stateless_ejemplos/lib/src/my_app.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:lesson5_ejemplos/src/pages/widgets_page.dart';
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WidgetsPage(),
+    );
+  }
+}
