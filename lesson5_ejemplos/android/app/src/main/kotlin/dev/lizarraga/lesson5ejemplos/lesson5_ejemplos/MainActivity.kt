cae0181a6a1731e7c450391aa3602dff86301670
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-12-16 18:48:46 -0400

    Change folder name

--- lesson5_ejemplos/android/app/src/main/kotlin/dev/lizarraga/lesson5ejemplos/lesson5_ejemplos/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.lizarraga.lesson5ejemplos.lesson5_ejemplos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lesson5_ejemplos/lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:lesson5_ejemplos/src/my_app.dart';
-
-void main() => runApp(MyApp());
-
