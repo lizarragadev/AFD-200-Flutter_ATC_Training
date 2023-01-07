e8c160658b9201957deb4c1984c8e4b27ffb71c4
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-01-06 22:22:30 -0400

    Add lesson 8

--- lesson8/lib/src/pages/expansion_panel_page.dart
+++ lesson8/lib/src/pages/expansion_panel_page.dart
@@ -7,25 +7,78 @@ class ExpansionPanelPage extends StatefulWidget {
   State<ExpansionPanelPage> createState() => _ExpansionPanelPageState();
 }
 
+class Item {
+  String title;
+  String description;
+  bool isExpanded;
+  Item({
+    required this.title,
+    required this.description,
+    this.isExpanded = false
+  });
+}
+
 class _ExpansionPanelPageState extends State<ExpansionPanelPage> {
+  final List<Item> data = List<Item>.generate(15, (index) {
+    return Item(
+      title: "Elemento número ${index + 1}",
+      description: "Esta es una descripcion que ira en la parte expandida de nuestro Item en una lista"
+    );
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Expansion Panel & Snackbar"),),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(mySnackbar("Red celular inestable"));
-              },
-              child: Text("Abrir Snackbar")
-          ),
-        ),
-      ),
+      body: creaListaExpandible(),
     );
   }
 
+  Widget creaListaExpandible() {
+    return ListView(
+      children: [
+        ExpansionPanelList(
+          children: data.map((item) {
+            return ExpansionPanel(
+              isExpanded: item.isExpanded,
+              headerBuilder: (context, isExpanded) {
+                  return ListTile(
+                    title: Text(item.title),
+                    onTap: () {
+                      ScaffoldMessenger.of(context).showSnackBar(mySnackbar("Red celular inestable"));
+                    },
+                  );
+                },
+                body: Column(
+                  children: [
+                    ListTile(
+                      title: Text(item.description),
+                      subtitle: Text("Este es un subtitulo"),
+                      trailing: Icon(Icons.delete),
+                      onTap: () {
+                        setState(() {
+                          data.removeWhere((element) => item == element);
+                        });
+                      },
+                    )
+                  ],
+                ),
+
+            );
+          }).toList(),
+          expansionCallback: (index, isExpanded) {
+            setState(() {
+              data[index].isExpanded = !isExpanded;
+            });
+          },
+        )
+      ],
+    );
+  }
+
+
+
+  // ScaffoldMessenger.of(context).showSnackBar(mySnackbar("Red celular inestable"));
   var mySnackbar = (mensaje) {
     return SnackBar(
         content: Row(
