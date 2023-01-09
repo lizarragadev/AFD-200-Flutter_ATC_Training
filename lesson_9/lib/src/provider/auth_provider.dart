5a7da9b489e4b5968e5b1d3e77578127355e3bba
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-01-09 18:48:08 -0400

    create providers

---
+++ lesson_9/lib/src/provider/auth_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AuthProvider {
+  BuildContext context;
+  AuthProvider({required this.context});
+
+  Future<dynamic> loginUsuario(correo, contrasenia) async {
+
+  }
+
+  Future<dynamic> registrarUsuario(correo, contrasenia) async {
+
+  }
+
+
+}
